3daa200a7e85a2f7b6b4a873228cab8732086f08
Felix Kurniawan Dharma <felixkd99@gmail.com>
2026-03-17 08:17:02 +0700

    User Register done

---
+++ SaasApplication/Controllers/Users/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTOs;
+using Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaasApplication.Controllers.Users
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        private readonly RegisterUserUseCase _registerUserUseCase;
+
+        public UserController(RegisterUserUseCase registerUserUseCase)
+        {
+            _registerUserUseCase = registerUserUseCase;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserRequest request)
+        {
+            var userId = await _registerUserUseCase.Execute(request);
+
+            return Ok(new { UserId = userId });
+        }
+    }
+}
